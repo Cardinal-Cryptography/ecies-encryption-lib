9a5095f4e43c427eade825ee0c85553b16a90f11
JanKuczma <jan.s.kuczma@gmail.com>
2025-07-25 14:42:03 +0200

    add lib dist

---
+++ ts/lib/dist/index.js
@@ -0,0 +1,100 @@
+import * as secp from "@noble/secp256k1";
+import { TextEncoder, TextDecoder } from "util";
+export function toHex(buf) {
+    return Buffer.from(buf).toString("hex");
+}
+export function fromHex(hex) {
+    return new Uint8Array(Buffer.from(hex, "hex"));
+}
+export async function getCrypto() {
+    return typeof globalThis.crypto !== "undefined"
+        ? globalThis.crypto
+        : (await import("node:crypto")).webcrypto;
+}
+export function generateKeypair() {
+    const sk = secp.utils.randomPrivateKey();
+    const pk = secp.getPublicKey(sk, true);
+    return { sk, pk };
+}
+async function hkdf(sharedSecret, cryptoAPI) {
+    const keyMaterial = await cryptoAPI.subtle.importKey("raw", sharedSecret, "HKDF", false, ["deriveKey"]);
+    return await cryptoAPI.subtle.deriveKey({
+        name: "HKDF",
+        hash: "SHA-256",
+        salt: new Uint8Array([]),
+        info: new TextEncoder().encode("ecies-secp256k1-v1"),
+    }, keyMaterial, { name: "AES-GCM", length: 256 }, false, ["encrypt", "decrypt"]);
+}
+async function _encrypt(message, recipientPubHex, cryptoAPI) {
+    const recipientPub = secp.Point.fromHex(recipientPubHex);
+    const ephSk = secp.utils.randomPrivateKey();
+    const ephPk = secp.getPublicKey(ephSk, true);
+    const ephSkBigInt = BigInt("0x" + toHex(ephSk));
+    const shared = recipientPub.multiply(ephSkBigInt).toRawBytes(true);
+    const aesKey = await hkdf(shared, cryptoAPI);
+    const iv = cryptoAPI.getRandomValues(new Uint8Array(12));
+    const ciphertextBuffer = await cryptoAPI.subtle.encrypt({ name: "AES-GCM", iv }, aesKey, message);
+    const ciphertext = new Uint8Array(ciphertextBuffer);
+    const out = new Uint8Array(ephPk.length + iv.length + ciphertext.length);
+    out.set(ephPk);
+    out.set(iv, ephPk.length);
+    out.set(ciphertext, ephPk.length + iv.length);
+    return out;
+}
+async function _decrypt(ciphertextBytes, recipientSkHex, cryptoAPI) {
+    const ephPk = secp.Point.fromHex(ciphertextBytes.slice(0, 33));
+    const iv = ciphertextBytes.slice(33, 45);
+    const ciphertext = ciphertextBytes.slice(45);
+    const skBytes = fromHex(recipientSkHex);
+    const skBigInt = BigInt("0x" + toHex(skBytes));
+    const shared_point = ephPk.multiply(skBigInt);
+    let shared = shared_point.toRawBytes(true);
+    const aesKey = await hkdf(shared, cryptoAPI);
+    const plaintextBuffer = await cryptoAPI.subtle.decrypt({ name: "AES-GCM", iv }, aesKey, ciphertext);
+    return new Uint8Array(plaintextBuffer);
+}
+export async function encrypt(message, recipientPubHex, cryptoAPI) {
+    const encoded = new TextEncoder().encode(message);
+    const out = await _encrypt(encoded, recipientPubHex, cryptoAPI);
+    return toHex(out);
+}
+export async function decrypt(ciphertextHex, recipientSkHex, cryptoAPI) {
+    const decrypted = await _decrypt(fromHex(ciphertextHex), recipientSkHex, cryptoAPI);
+    return new TextDecoder().decode(decrypted);
+}
+export async function encryptPadded(message, recipientPubHex, cryptoAPI, paddedLength) {
+    if (paddedLength < message.length + 4) {
+        throw new Error(`Invalid padded length ${paddedLength} bytes, expected at least ${message.length + 4} bytes)`);
+    }
+    let encoded = new Uint8Array(paddedLength);
+    // prepend with the message length info in little endian (4 bytes)
+    const buffer = new ArrayBuffer(4);
+    const view = new DataView(buffer);
+    view.setUint32(0, message.length, true);
+    encoded.set(new Uint8Array(buffer), 0);
+    encoded.set(new TextEncoder().encode(message), 4);
+    const encrypted = await _encrypt(encoded, recipientPubHex, cryptoAPI);
+    return toHex(encrypted);
+}
+export async function decryptPadded(ciphertextHex, recipientSkHex, cryptoAPI, paddedLength) {
+    const decrypted = await _decrypt(fromHex(ciphertextHex), recipientSkHex, cryptoAPI);
+    if (decrypted.length != paddedLength) {
+        throw new Error(`Invalid padded length ${decrypted.length} bytes, expected ${paddedLength} bytes)`);
+    }
+    return decodePadded(decrypted);
+}
+export async function decryptPaddedUnchecked(ciphertextHex, recipientSkHex, cryptoAPI) {
+    const decrypted = await _decrypt(fromHex(ciphertextHex), recipientSkHex, cryptoAPI);
+    return await decodePadded(decrypted);
+}
+async function decodePadded(paddedMessage) {
+    if (paddedMessage.length < 4) {
+        throw new Error(`Invalid padded length ${paddedMessage.length} bytes, expected at least ${4} bytes)`);
+    }
+    const view = new DataView(paddedMessage.buffer);
+    const messageLength = view.getUint32(0, true);
+    if (messageLength > paddedMessage.length - 4) {
+        throw new Error(`Invalid message length ${messageLength} bytes, expected at most ${paddedMessage.length - 4} bytes)`);
+    }
+    return new TextDecoder().decode(paddedMessage.subarray(4, messageLength + 4));
+}
